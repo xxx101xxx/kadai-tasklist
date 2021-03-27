25aadbdb806e43d40240db2a06268cb905b22c7d
xxx <ki474262@gmail.com>
2021-03-27 07:40:24 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,14 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
- 
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
   
   def index
-      @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
+      @tasks = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
-
+  
   def show
-      @task = Task.find(params[:id])
        
   end
 
