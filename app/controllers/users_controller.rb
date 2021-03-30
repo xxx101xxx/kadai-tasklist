60113b86a88c16904fbf5f2bce3deb2ee525266a
xxx <ki474262@gmail.com>
2021-03-30 06:53:25 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, ]
-
-  def index
-    @users = User.all.page(params[:page])
-  end
-
-  def show
-     @user = User.find(params[:id])
-     
-    
-  end
 
   def new
      @user = User.new
